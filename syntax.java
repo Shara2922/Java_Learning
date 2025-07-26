66e7d1ba3d11d2422e139210392d0067d44fec30
Shara2922 <r.shara2922@gmail.com>
2025-07-25 20:04:50 -0500

    java syntax

---
+++ syntax.java
@@ -0,0 +1,17 @@
+// In the previous chapter, we created a Java file called Main.java, 
+// and we used the following code to print "Hello World" to the screen:
+public class Main { //The class name should always start with an uppercase. ex: class Main.
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
+
+/* The curly braces {} marks the beginning and the end of a block of code.
+Text must be wrapped inside double quotations marks "".
+System.out.println(This sentence will produce an error);
+
+There is also a print() method, which is similar to println().
+The only difference is that it does not insert a new line at the end of the output
+
+System.out.print("Hello World! ");
+System.out.print("I will print on the same line.");
