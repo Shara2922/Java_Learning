0c6ff98fd5415101738ffa2baa65145180fb73e4
Shara2922 <r.shara2922@gmail.com>
2025-08-01 23:59:23 -0500

    DoWhileLoops

---
+++ Loops/DoWhileLoops.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class DoWhileLoopExample {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        // Keep asking until user types "exit"
+        do {
+            System.out.print("Enter something (type 'exit' to quit): ");
+            input = scanner.nextLine();
+        } while (!input.equalsIgnoreCase("exit"));
+
+        System.out.println("Exited loop.");
+    }
+}
