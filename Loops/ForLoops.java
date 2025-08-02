6efdb498880fc44af87d3ce9e36a963fff6b887f
Shara2922 <r.shara2922@gmail.com>
2025-08-01 23:53:04 -0500

    ForLoops

---
+++ Loops/ForLoops.java
@@ -0,0 +1,8 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        // Print numbers from 1 to 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Number: " + i);
+        }
+    }
+}
