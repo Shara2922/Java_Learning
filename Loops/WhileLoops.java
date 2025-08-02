d94715b8b22453036ea3b74b76b5f3ab96ca584f
Shara2922 <r.shara2922@gmail.com>
2025-08-01 23:56:02 -0500

    WhileLoops

---
+++ Loops/WhileLoops.java
@@ -0,0 +1,10 @@
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        // Print even numbers from 2 to 20
+        int number = 2;
+        while (number <= 20) {
+            System.out.println("Even Number: " + number);
+            number += 2;
+        }
+    }
+}
