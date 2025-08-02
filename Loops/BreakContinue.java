f9a1718f16926f940b3e52478a34efa877afdbd4
Shara2922 <r.shara2922@gmail.com>
2025-08-02 00:03:00 -0500

    BreakContinue

---
+++ Loops/BreakContinue.java
@@ -0,0 +1,14 @@
+public class BreakContinueExample {
+    public static void main(String[] args) {
+        // Print numbers from 1 to 10, skip 5, stop at 8
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                continue; // skip 5
+            }
+            if (i == 8) {
+                break; // stop loop
+            }
+            System.out.println("i = " + i);
+        }
+    }
+}
